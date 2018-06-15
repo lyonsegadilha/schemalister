9852001c9cb98a06ebc83e545802a380dc398ab8
lyonsegadilha <lyonborgessegadilha@gmail.com>
2018-06-15 18:31:23 -0300

    variaveis

--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("SALESFORCE_CONSUMER_KEY", "schemalister.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schemalister.settings")
+    os.environ.setdefault("SALESFORCE_CONSUMER_KEY", "schemalistter.setings")
     os.environ.setdefault("SALESFORCE_CONSUMER_SECRET", "schemalister.settings")
     os.environ.setdefault("SALESFORCE_API_VERSION", "schemalister.settings")
 
