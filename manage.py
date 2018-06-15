bd3f42a4a7713ce1236df427ea2d573673b2d9c7
lyonsegadilha <lyonborgessegadilha@gmail.com>
2018-06-15 18:33:48 -0300

    variaveis

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "schemalister.settings")
     os.environ.setdefault("SALESFORCE_CONSUMER_KEY", "schemalistter.setings")
     os.environ.setdefault("SALESFORCE_CONSUMER_SECRET", "schemalister.settings")
-    os.environ.setdefault("SALESFORCE_API_VERSION", "schemalister.settings")
     os.environ.setdefault("SALESFORCE_API_VERSION", "41")
 
     from django.core.management import execute_from_command_line
